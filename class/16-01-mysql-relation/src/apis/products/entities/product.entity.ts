ac883be211d00a1d0c9e7a1daf37e5a68ef00d60
jjong981028 <jjong981028@gmail.com>
2022-06-29 20:24:19 +0900

    day16

---
+++ class/16-01-mysql-relation/src/apis/products/entities/product.entity.ts
@@ -0,0 +1,46 @@
+import { ProductCategory } from 'src/apis/productsCategory/entities/productCategory.entity';
+import { ProductSaleslocation } from 'src/apis/productsSaleslocation/entities/productSaleslocation.entity';
+import { ProductTag } from 'src/apis/productTag/entities/productTag.entity';
+import { User } from 'src/apis/user/entities/user.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  isSoldout: boolean;
+
+  @JoinColumn() //OneToOne 매핑 일때는 joincolumn붙여 줘야 한다! => graphql한테 알려주는 용도 아님!! =>기준을 정한 곳에 적어 놓는 것
+  @OneToOne(() => ProductSaleslocation)
+  productSaleslocation: ProductSaleslocation;
+
+  @ManyToOne(() => ProductCategory)
+  productCategory: ProductCategory;
+
+  @ManyToOne(() => User)
+  user: User;
+
+  @JoinTable()
+  @ManyToMany(() => ProductTag, (productTags) => productTags.products)
+  productTags: ProductTag[];
+}
